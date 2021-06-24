c1a7d8825ed70ef9e3a5c2c7803043d38629054a
Saleem Younus <saleemyounus@gmail.com>
2021-06-24 14:46:39 -0500

    learning python

---
+++ app.py
@@ -0,0 +1,14 @@
+print("Saleem Younus")
+print('o----')
+print(' ||||')
+print('*'*10)
+
+price = 10
+rating = 4.9
+name = 'Saleem'
+is_published = True
+print(price)
+
+fullname = "john smith"
+age = 20
+is_new = True
